a52ef5d62160a930b4c9268b3b4db69a2495912f
Froskk <37559715+Froskk@users.noreply.github.com>
2020-01-05 16:44:29 +0000

    Unlink Chart from data + loading indicators

--- src/Chart.js
+++ src/Chart.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { useFetch } from "./useFetch";
 import {
   LineChart,
   Line,
@@ -9,54 +8,36 @@ import {
   Tooltip
 } from "recharts";
 import { curveCatmullRom } from "d3-shape";
-const apiURL = "https://coachdeq.rossc.now.sh/api/get-data";
 
 export const BaseChart = ({ data }) => {
-  const { singleSeries, loading } = useFetch(apiURL);
-
   return (
-    <div>
-      {loading && <div>Loading...</div>}
-
-      {!loading && (
-        <LineChart
-          width={400}
-          height={300}
-          data={singleSeries}
-          margin={{
-            top: 5,
-            right: 60,
-            left: 20,
-            bottom: 10
-          }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip content={() => <div />} />
+    <LineChart
+      width={400}
+      height={300}
+      data={data}
+      margin={{
+        top: 5,
+        right: 60,
+        left: 20,
+        bottom: 10
+      }}
+    >
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="date" />
+      <YAxis />
+      <Tooltip content={() => <div />} />
+      {Object.keys(data[0]).map(measure =>
+        measure !== "date" ? (
           <Line
             type={curveCatmullRom}
-            dataKey="x"
-            stroke="#F5AED6"
+            dataKey={measure}
+            stroke={measure === "average" ? "#8BD5E6" : "#FFC600"}
             strokeWidth="2"
             dot={false}
+            key={measure}
           />
-          <Line
-            type={curveCatmullRom}
-            dataKey="reading"
-            stroke="#F5A862"
-            strokeWidth="2"
-            dot={false}
-          />
-          <Line
-            type={curveCatmullRom}
-            dataKey="average"
-            stroke="#8BD5E6"
-            strokeWidth="2"
-            dot={false}
-          />
-        </LineChart>
+        ) : null
       )}
-    </div>
+    </LineChart>
   );
 };
